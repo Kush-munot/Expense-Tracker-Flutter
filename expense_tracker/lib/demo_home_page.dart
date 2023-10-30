42ace54e080c80520e743c59a4a934fbb070a33e
Kush Munot <munot.kgm@gmail.com>
2023-10-30 10:03:31 +0530

    Final project ready

--- expense_tracker/lib/demo_home_page.dart
+++ expense_tracker/lib/demo_home_page.dart
@@ -5,95 +5,6 @@ import 'package:expense_tracker/top_card.dart';
 import 'package:expense_tracker/transaction.dart';
 import 'package:flutter/material.dart';
 
-/* class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  List<Transaction> transactions = []; // List to store fetched transactions
-
-  void _newTransaction() {
-    showDialog(
-        context: context,
-        builder: (context) {
-          return const AlertDialog(
-            title: Text("New Transaction"),
-            content: Text("Hello World"),
-          );
-        });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    fetchData();
-  }
-
-  Future<void> fetchData() async {
-    const apiUrl =
-        'https://script.google.com/macros/s/AKfycbzAmPGWoG6N9cE2fxOFyWPoRu5rGu3gcJsmVajmbhgNet-xAeqC5_CeNZSx3EpdMwSN/exec';
-
-    try {
-      final response = await http.get(Uri.parse(apiUrl));
-      if (response.statusCode == 200) {
-        final List<dynamic> data = json.decode(response.body);
-        final List<Transaction> fetchedTransactions = data.map((item) {
-          return Transaction(
-            transactionName: item['Message'],
-            money: item['Amount'].toString(),
-            expenseOrIncome: item['Expense/Income'],
-            category: item['Category'],
-            message: item['Message'],
-          );
-        }).toList();
-
-        setState(() {
-          transactions = fetchedTransactions;
-        });
-      } else {
-        print('Failed to fetch data. Status Code: ${response.statusCode}');
-      }
-    } catch (e) {
-      print('Error: $e');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: ListView(
-        shrinkWrap: true,
-        children: <Widget>[
-          TopCard(balance: "₹ 100", income: "₹ 300", expense: "₹ 400"),
-          Expanded(
-            child: Center(
-              child: Column(
-                children: transactions.map((transaction) {
-                  return Transaction(
-                    transactionName: transaction.transactionName,
-                    money: transaction.money,
-                    expenseOrIncome: transaction.expenseOrIncome,
-                    category: transaction.category,
-                    message: transaction.message,
-                  );
-                }).toList(),
-              ),
-            ),
-          ),
-          PlusButton(
-            function: _newTransaction,
-          ),
-        ],
-      ),
-    );
-  }
-}
- */
-
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -104,12 +15,16 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   List<Transaction> transactions = []; // List to store fetched transactions
 
+  String balance = "₹ 0";
+  String income = "₹ 0";
+  String expense = "₹ 0";
+
   void _newTransaction() {
-    String expenseOrIncome = 'Expense';
+    String expenseOrIncome = 'Select';
     String title = '';
     String amount = '';
     String message = '';
-    String category = 'Milk';
+    String category = 'Category';
 
     showDialog(
       context: context,
@@ -134,12 +49,6 @@ class _HomePageState extends State<HomePage> {
                     });
                   },
                 ),
-                /* TextFormField(
-                  decoration: const InputDecoration(labelText: 'Title'),
-                  onChanged: (value) {
-                    title = value;
-                  },
-                ), */
                 TextFormField(
                   decoration: const InputDecoration(labelText: 'Amount'),
                   onChanged: (value) {
@@ -234,8 +143,24 @@ class _HomePageState extends State<HomePage> {
           );
         }).toList();
 
+        // Calculate balance, income, and expense
+        double totalIncome = 0;
+        double totalExpense = 0;
+
+        for (var transaction in fetchedTransactions) {
+          if (transaction.expenseOrIncome == 'Income') {
+            totalIncome += double.parse(transaction.money);
+          } else if (transaction.expenseOrIncome == 'Expense') {
+            totalExpense += double.parse(transaction.money);
+          }
+        }
+
+        // Update state variables
         setState(() {
           transactions = fetchedTransactions;
+          income = '₹ $totalIncome';
+          expense = '₹ $totalExpense';
+          balance = '₹ ${totalIncome - totalExpense}';
         });
       } else {
         print('Failed to fetch data. Status Code: ${response.statusCode}');
@@ -280,7 +205,7 @@ class _HomePageState extends State<HomePage> {
       body: ListView(
         shrinkWrap: true,
         children: <Widget>[
-          TopCard(balance: "₹ 100", income: "₹ 300", expense: "₹ 400"),
+          TopCard(balance: balance, income: income, expense: expense),
           Center(
             child: Column(
               children: transactions.map((transaction) {
--- expense_tracker/lib/google_sheets_api.dart
+++
@@ -1,6 +0,0 @@
-class GoogleSheets{
-  static int numberOfTransactions = 0;
-  static List<List<dynamic>> currentTransactions = [];
-  static bool loading = true;
-
-}
